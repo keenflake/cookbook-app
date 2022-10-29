cf41be3416decd7d4a6b9860c8e79f9652f04dcf
Aleksey Kuznetsov <keenflake@gmail.com>
2022-10-29 12:56:35 +0400

    refactor: add paths support

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { AppProps } from 'next/app';
 import { FC } from 'react';
 
-import '../styles/main.css';
+import '@app/styles/main.css';
 
 const App: FC<AppProps> = ({ Component, pageProps }) => <Component {...pageProps} />;
 
