cb36abd553e37a6d4ae028171892a7bad892a57f
Aleksey Kuznetsov <keenflake@gmail.com>
2022-10-29 12:54:31 +0400

    build: add tailwind support

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import { NextPage } from 'next';
 
 const IndexPage: NextPage = () => {
-  return <div>it works!</div>;
+  return <div className="p-4 bg-slate-200 text-gray-700">it works!</div>;
 };
 
 export default IndexPage;
