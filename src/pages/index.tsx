b87221369549c1066c25eba5dc930da175ce6dd9
Aleksey Kuznetsov <keenflake@gmail.com>
2022-11-02 20:58:49 +0400

    feat: add search capabilities on index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
+import { Form, Formik } from 'formik';
 import { GetServerSideProps, NextPage } from 'next';
 import { unstable_getServerSession } from 'next-auth';
+import { useCallback, useState } from 'react';
 
 import { getAllRecipes } from '@app/api/supabase/queries';
-import { Container, RecipeCard } from '@app/common/components';
+import { Button, Container, RecipeCard, TextField } from '@app/common/components';
 import { Recipe } from '@app/common/models';
 
 import { authOptions } from './api/auth/[...nextauth]';
@@ -11,19 +13,67 @@ interface IndexPageProps {
   recipes: Recipe[];
 }
 
-const IndexPage: NextPage<IndexPageProps> = ({ recipes }) => {
+interface SearchValues {
+  query: string;
+}
+
+const searchInitialValue: SearchValues = {
+  query: '',
+};
+
+const IndexPage: NextPage<IndexPageProps> = ({ recipes: initialRecipes }) => {
+  const [filteredRecipes, setFilteredRecipes] = useState(initialRecipes);
+
+  const handleSubmit = useCallback(
+    (values: SearchValues) => {
+      setFilteredRecipes(() => {
+        if (!values.query) {
+          return initialRecipes;
+        }
+
+        const query = values.query.toLowerCase();
+
+        return initialRecipes.filter(
+          recipe =>
+            recipe.name.toLowerCase().includes(query) ||
+            recipe.description.toLowerCase().includes(query) ||
+            recipe.cuisine.toLowerCase().includes(query),
+        );
+      });
+    },
+    [initialRecipes],
+  );
+
   return (
     <Container className="mb-12">
       <h2>Latest</h2>
 
-      {recipes.length > 0 && (
-        <ul className="grid grid-cols-2 gap-x-4 gap-y-3">
-          {recipes.map(recipe => (
-            <li key={recipe.id}>
-              <RecipeCard recipe={recipe} />
-            </li>
-          ))}
-        </ul>
+      {initialRecipes.length > 0 ? (
+        <>
+          <Formik initialValues={searchInitialValue} onSubmit={handleSubmit}>
+            <Form className="flex space-x-4 mb-6">
+              <TextField
+                name="query"
+                id="query"
+                placeholder="Lasagna Bolognese"
+                onChange={(...values) => console.log(values)}
+              />
+              <Button type="submit" appearance="primary" className="px-8">
+                Search
+              </Button>
+            </Form>
+          </Formik>
+
+          <ul className="grid grid-cols-2 gap-x-4 gap-y-3">
+            {filteredRecipes.map(recipe => (
+              <li key={recipe.id}>
+                <RecipeCard recipe={recipe} />
+              </li>
+            ))}
+          </ul>
+        </>
+      ) : (
+        <p>No recipes found</p>
       )}
     </Container>
   );
